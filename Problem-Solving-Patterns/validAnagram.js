80d32b6b1e182ed6f76d97af52a807bb484da6f5
Ha-Mundo <trauzzu@gmail.com>
2023-09-01 10:46:39 +0100

    Valid Anagram Challenge Added

---
+++ Problem-Solving-Patterns/validAnagram.js
@@ -0,0 +1,11 @@
+/////////////// Frequency Counter Coding Exercise 1: - validAnagram //////////////////////////////
+
+// Given two strings, write a function to determine if the second string is an anagram of the first.
+// An anagram is a word, phrase, or name formed by rearranging the letters of another, such as cinema,
+// formed from iceman.
+
+function validAnagram(str1, str2) {}
+
+console.log(validAnagram("", "")); // true
+console.log(validAnagram("anagram", "nagaram")); // true
+console.log(validAnagram("aaz", "zza")); // false
