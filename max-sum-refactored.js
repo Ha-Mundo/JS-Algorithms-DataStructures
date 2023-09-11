46ee566c9b9b8c91a18817b500adf9b5e2582c1d
Ha-Mundo <trauzzu@gmail.com>
2023-09-11 21:03:29 +0100

    Coding samples added

---
+++ max-sum-refactored.js
@@ -0,0 +1,24 @@
+/////////////// SLIDING WINDOW Coding sample: - maxSubarraySum//////////////////////////////
+
+//Write a function called maxSubarraySum which accepts an array of integers and a number called n. The function should calculate the maximum sum of n consecutive elements in the array.
+
+//Time Complexity - O(N)
+function maxSubarraySum(arr, num) {
+  let maxSum = 0;
+  let tempSum = 0;
+  if (arr.length < num) return null;
+  for (let i = 0; i < num; i++) {
+    maxSum += arr[i];
+  }
+  tempSum = maxSum;
+  for (let i = num; i < arr.length; i++) {
+    tempSum = tempSum - arr[i - num] + arr[i];
+    maxSum = Math.max(maxSum, tempSum);
+  }
+  return maxSum;
+}
+
+maxSubarraySum([2, 6, 9, 2, 1, 8, 5, 6, 3], 3);
+maxSubarraySum([4, 2, 1, 6], 1); // 6
+maxSubarraySum([4, 2, 1, 6, 2], 4); // 13
+maxSubarraySum([], 4); // null
