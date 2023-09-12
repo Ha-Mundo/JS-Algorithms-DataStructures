fe0cf3111d7a7f94204d39dc503b91172110ec5e
Ha-Mundo <trauzzu@gmail.com>
2023-09-12 12:30:57 +0100

    comment update

--- Problem-Solving-Patterns/search-naive.js
+++ Problem-Solving-Patterns/search-naive.js
@@ -1,6 +1,6 @@
 //Given a sorted array of integers, write a function called search, that accepts a value and returns the index where the value passed to the function is located. If the value is not found, return -1
 
-//Time Complexity O(N)
+//Time Complexity O(N) - Linear Search
 function search(arr, val) {
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] === val) {
