4e49513cfb6790c30665d8b0c2081531f08f2351
Ha-Mundo <trauzzu@gmail.com>
2023-09-12 10:08:31 +0100

    new comment added

--- max-sum-refactored.js
+++ max-sum-refactored.js
@@ -12,7 +12,7 @@ function maxSubarraySum(arr, num) {
   }
   tempSum = maxSum;
   for (let i = num; i < arr.length; i++) {
-    tempSum = tempSum - arr[i - num] + arr[i];
+    tempSum = tempSum - arr[i - num] + arr[i]; // <-- Sliding window
     maxSum = Math.max(maxSum, tempSum);
   }
   return maxSum;
