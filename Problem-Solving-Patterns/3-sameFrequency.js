24505a114ce692f787b6e70f2748f74725bd15a2
Ha-Mundo <trauzzu@gmail.com>
2023-09-12 17:46:28 +0100

    frequency counter exercises added

---
+++ Problem-Solving-Patterns/3-sameFrequency.js
@@ -0,0 +1,11 @@
+//////// Frequency Counter Coding Exercise 3: - sameFrequency //////////////////////
+
+// Write a function called sameFrequency. Given two positive integers, find out if the two
+// numbers have the same frequency of digits.
+
+// Your solution MUST have the following complexities: Time: O(N)
+
+function sameFrequency(num1, num2) {}
+
+console.log(sameFrequency(182, 281)); // true
+console.log(sameFrequency(22, 222)); // false
---
+++ Problem-Solving-Patterns/4-areThereDuplicates.js
@@ -0,0 +1,11 @@
+/////// Frequency Counter / Multiple Pointers Coding Exercise 4: - areThereDuplicates  /////////////
+
+// Implement a function called, areThereDuplicates  which accepts a variable number of arguments, and
+// checks whether there are any duplicates among the arguments passed in.  You can solve this using the
+// frequency counter pattern OR the multiple pointers pattern.
+
+////////   Frequency Counter solution //////////
+function areThereDuplicates() {}
+
+console.log(areThereDuplicates("a", "b", "c", "a")); // true;
+console.log(areThereDuplicates("a", "b", "c", "d")); // false
