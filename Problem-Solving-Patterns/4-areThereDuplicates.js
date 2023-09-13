086905e1264787c55c3dbf280336cb650a4dc836
Ha-Mundo <trauzzu@gmail.com>
2023-09-13 21:00:14 +0100

    Multiple Pointers solution added

--- Problem-Solving-Patterns/4-areThereDuplicates.js
+++ Problem-Solving-Patterns/4-areThereDuplicates.js
@@ -1,10 +1,8 @@
-/////// Frequency Counter / Multiple Pointers Coding Exercise 4: - areThereDuplicates  /////////////
+/////// Frequency Counter / Multiple Pointers Coding Exercise 4: - areThereDuplicates /////////////
 
-// Implement a function called, areThereDuplicates  which accepts a variable number of arguments, and
-// checks whether there are any duplicates among the arguments passed in. You can solve this using the
-// frequency counter pattern OR the multiple pointers pattern.
+// Implement a function called, areThereDuplicates which accepts a variable number of arguments, and checks whether there are any duplicates among the arguments passed in. You can solve this using the frequency counter pattern OR the multiple pointers pattern.
 
-////////   Frequency Counter solution //////////
+//////// Frequency Counter solution //////////
 function areThereDuplicates(...args) {
   let frequencyCounter = {};
 
@@ -17,5 +15,22 @@ function areThereDuplicates(...args) {
   return false;
 }
 
-console.log(areThereDuplicates("a", "b", "c", "a")); // true;
-console.log(areThereDuplicates("a", "b", "c", "d")); // false
+//////// Multiple Pointers solution //////////
+function areThereDuplicates(...args) {
+  let sortedArr = Array.from(args).sort();
+
+  let j = 1;
+  for (let index in sortedArr) {
+    if (sortedArr[index] === sortedArr[j]) {
+      return true;
+    } else {
+      j++;
+    }
+  }
+  return false;
+}
+
+//console.log(areThereDuplicates("a", "2", "3", "1", "3")); // true
+//console.log(areThereDuplicates("b","a","c","a" )); // true;
+//console.log(areThereDuplicates("a", "b", "c", "d")); // false
+//console.log(areThereDuplicates(1,3,2,5,4 )); // false;
