53fbab743406428e9ad3839b630c2426ec077863
Ha-Mundo <trauzzu@gmail.com>
2023-09-13 17:38:34 +0100

    areThereDuplicates Frequency Counter solution added

--- Problem-Solving-Patterns/4-areThereDuplicates.js
+++ Problem-Solving-Patterns/4-areThereDuplicates.js
@@ -1,11 +1,21 @@
 /////// Frequency Counter / Multiple Pointers Coding Exercise 4: - areThereDuplicates  /////////////
 
 // Implement a function called, areThereDuplicates  which accepts a variable number of arguments, and
-// checks whether there are any duplicates among the arguments passed in.  You can solve this using the
+// checks whether there are any duplicates among the arguments passed in. You can solve this using the
 // frequency counter pattern OR the multiple pointers pattern.
 
 ////////   Frequency Counter solution //////////
-function areThereDuplicates() {}
+function areThereDuplicates(...args) {
+  let frequencyCounter = {};
+
+  for (let value of args) {
+    frequencyCounter[value] = (frequencyCounter[value] || 0) + 1;
+  }
+  for (key in frequencyCounter) {
+    if (frequencyCounter[key] > 1) return true;
+  }
+  return false;
+}
 
 console.log(areThereDuplicates("a", "b", "c", "a")); // true;
 console.log(areThereDuplicates("a", "b", "c", "d")); // false
