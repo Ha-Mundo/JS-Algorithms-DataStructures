70943c5d644447b637c78950617eea56a0110836
Ha-Mundo <trauzzu@gmail.com>
2023-09-18 09:56:00 +0100

    Multiple Pointers Coding Exercise 6 completed

--- Problem-Solving-Patterns/6-isSubsequence.js
+++ Problem-Solving-Patterns/6-isSubsequence.js
@@ -6,7 +6,18 @@
 // Time Complexity - O(N + M) <-- If M is the length of message and N is the length of letters
 // Space Complexity - O(1)
 
-function isSubsequence(str1, str2) {}
+function isSubsequence(str1, str2) {
+  let i = 0;
+  let j = 0;
+  if (!str1) return true;
+
+  while (j < str2.length) {
+    if (str1[i] === str2[j]) i++;
+    if (i === str1.length) return true;
+    j++;
+  }
+  return false;
+}
 
 console.log(isSubsequence("sing", "sting")); // true
 console.log(isSubsequence("abc", "abracadabra")); // true
