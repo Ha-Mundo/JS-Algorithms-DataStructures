12df91708e3c31ebbbd72259d5508f8ff04cd36c
Ha-Mundo <trauzzu@gmail.com>
2023-09-20 17:45:54 +0100

    new solution added

--- Problem-Solving-Patterns/8-minSubArrayLen.js
+++ Problem-Solving-Patterns/8-minSubArrayLen.js
@@ -2,25 +2,47 @@
 
 // Write a function called minSubArrayLen  which accepts two parameters - an array of positive integers and a positive integer.This function should return the minimal length of a contiguous  subarray of which the sum is greater than or equal to the integer passed to the function. If there isn't one, return 0 instead.
 
+//Time complexity: O(n)
+//Space complexity: O(1)
 function minSubArrayLen(nums, sum) {
-  let total = 0;
-  let start = 0;
-  let end = 0;
+  let windowSum = 0;
+  let windowStart = 0;
   let minLen = Infinity;
 
-  while (start < nums.length) {
+  for (let windowEnd = 0; windowEnd < nums.length; windowEnd++) {
+    windowSum += nums[windowEnd];
+    // shrink the window until the windowSum is smaller than sum
+    while (windowSum >= sum) {
+      minLen = Math.min(minLen, windowEnd - windowStart + 1);
+      // subtract the element at the windowStart index
+      windowSum -= nums[windowStart];
+      // change windowStart to the next element
+      windowStart++;
+    }
+  }
+  return minLen === Infinity ? 0 : minLen;
+}
+
+////////////// refactored version ////////////////
+function minSubArrayLen(nums, sum) {
+  let windowSum = 0;
+  let windowStart = 0;
+  let windowEnd = 0;
+  let minLen = Infinity;
+
+  while (windowStart < nums.length) {
     // if current window doesn't add up to the given sum then move the window to right
-    if (total < sum && end < nums.length) {
-      total += nums[end];
-      end++;
+    if (windowSum < sum && windowEnd < nums.length) {
+      windowSum += nums[windowEnd];
+      windowEnd++;
     }
     // if current window adds up to at least the sum given then we can shrink the window
-    else if (total >= sum) {
-      minLen = Math.min(minLen, end - start);
-      total -= nums[start];
-      start++;
+    else if (windowSum >= sum) {
+      minLen = Math.min(minLen, windowEnd - windowStart);
+      windowSum -= nums[windowStart];
+      windowStart++;
     }
-    // current total less than required total but we reach the end, need this or else we'll be in an infinite loop
+    // current windowSum less than required windowSum but we reach the windowEnd, need this or else we'll be in an infinite loop
     else {
       break;
     }
