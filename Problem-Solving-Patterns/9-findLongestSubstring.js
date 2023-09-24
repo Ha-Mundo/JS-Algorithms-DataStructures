1d0c18a444240cda94ded9e329b7e2c7bcd56ea9
Ha-Mundo <trauzzu@gmail.com>
2023-09-24 20:03:38 +0100

    Sliding Window Coding Exercise 9 updated

--- Problem-Solving-Patterns/9-findLongestSubstring.js
+++ Problem-Solving-Patterns/9-findLongestSubstring.js
@@ -2,7 +2,24 @@
 
 // Write a function called findLongestSubstring, which accepts a string and returns the length of the longest substring with all distinct characters.
 
-function findLongestSubstring(str) {}
+function findLongestSubstring(str) {
+  let windowStart = 0;
+  let lookup = {};
+  let maxLen = 0;
+
+  for (let windowEnd = 0; windowEnd < str.length; windowEnd++) {
+    let char = str[windowEnd];
+    // if the letter is already present inside the string shrink the window and move the windowStart pointer to the next index
+    if (lookup[char]) {
+      windowStart = Math.max(windowStart, lookup[char]);
+    }
+    // index - beginning of substring + 1 (to include current in count)
+    maxLen = Math.max(maxLen, windowEnd - windowStart + 1);
+    // store the index of the next char so as to not double count
+    lookup[char] = windowEnd + 1;
+  }
+  return maxLen;
+}
 
 console.log(findLongestSubstring("rithmschool")); // 7
 console.log(findLongestSubstring("bbb")); // 1
