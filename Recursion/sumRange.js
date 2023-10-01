50d107ee097e37d80edc981b49100ddc4fb93fad
Ha-Mundo <trauzzu@gmail.com>
2023-10-01 20:13:29 +0100

    basic recursive samples added

---
+++ Recursion/sumRange.js
@@ -0,0 +1,10 @@
+function sumRange(num) {
+  if (num === 1) return 1;
+  return num + sumRange(num - 1);
+}
+
+/*  sumRange(3)
+    return 3 + sumRange(2)  <-- return 3 + 3
+        return 2 + sumRange(1)  <--  return 2 + 1
+            return 1  <-- return 1
+ */
