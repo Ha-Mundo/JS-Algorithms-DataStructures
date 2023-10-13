27c138a235fcf23037791860c1a57d199a8b47ea
Ha-Mundo <trauzzu@gmail.com>
2023-10-13 21:30:53 +0100

    Recursion Coding Exercise 10 Completed

--- Recursion/10-power.js
+++ Recursion/10-power.js
@@ -4,8 +4,19 @@
    of the base to the exponent. This function should mimic the functionality of Math.pow()   
    - do not worry about negative bases and exponents. */
 
-function power(base, exp) {}
+function power(base, exp) {
+  if (exp === 0) return 1;
+  return base * power(base, exp - 1);
+}
 
-// // power(2,0) // 1
-// // power(2, 2); // 4
-// // power(2,4) // 16
+// power(2, 0) // 1
+// power(2, 2); // 4
+// power(2, 4) // 16
+
+/* 
+    power(2, 4)
+    return 2 * power(2, 3)  <-- 2 * 8 = 16
+         return 2 * power(2, 2)  <-- 2 * 4 = 8
+             return 2 * power(2, 1)  <-- 2 * 2 = 4
+                 return 2 * power(2, 0) <-- 2 * 1 = 2
+    */
