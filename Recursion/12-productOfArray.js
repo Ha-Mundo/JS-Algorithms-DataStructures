7dd6367d2fd085eed55325f29c065f953e36dea2
Ha-Mundo <trauzzu@gmail.com>
2023-10-13 21:51:43 +0100

    Recursion Coding Exercise 12 Added

---
+++ Recursion/12-productOfArray.js
@@ -0,0 +1,8 @@
+///////// Recursion Coding Exercise 12: productOfArray //////////////////
+
+// Write a function called productOfArray which takes in an array of numbers and returns the product of them all.
+
+function productOfArray(arr) {}
+
+// productOfArray([1, 2, 3, 10])) //60
+// productOfArray([1,2,3]) // 6
