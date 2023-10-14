88a575e419fc6f8f02a1287fad2cefa067c506f3
Ha-Mundo <trauzzu@gmail.com>
2023-10-14 17:52:05 +0100

    Pure Recursion Solution Added

--- Recursion/12-productOfArray.js
+++ Recursion/12-productOfArray.js
@@ -19,5 +19,20 @@ function productOfArray(arr) {
   return result;
 }
 
-// productOfArray([1, 2, 3, 10])) //60
-// productOfArray([1,2,3]) // 6
+productOfArray([1, 2, 3]); // 6
+
+//// Pure Recursion ////
+function productOfArray(arr) {
+  if (arr.length === 0) return 1;
+
+  return (arr[0] *= productOfArray(arr.slice(1)));
+}
+
+productOfArray([1, 2, 3, 10]); //60
+/*
+1 * productOfArray([2, 3, 10])  <-- 1 * 60 = 60
+    2 * productOfArray([3, 10])  <-- 2 * 30 = 60
+        3 * productOfArray([10])  <-- 3 * 10 = 30
+            10 * productOfArray([])  <-- 10 * 1 = 10
+                1
+*/
