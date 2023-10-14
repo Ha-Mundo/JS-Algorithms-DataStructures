00732a6d2e92cf40befcedfdb666b5bfb816f56b
Ha-Mundo <trauzzu@gmail.com>
2023-10-14 16:55:49 +0100

    Helper Method Recursion Solution Added

--- Recursion/12-productOfArray.js
+++ Recursion/12-productOfArray.js
@@ -2,7 +2,22 @@
 
 // Write a function called productOfArray which takes in an array of numbers and returns the product of them all.
 
-function productOfArray(arr) {}
+///// Helper Method Recursion //////
+function productOfArray(arr) {
+  let result = 1;
+
+  function helper(helperInput) {
+    if (helperInput.length === 0) return;
+
+    result *= helperInput[0];
+
+    helper(helperInput.slice(1));
+  }
+
+  helper(arr);
+
+  return result;
+}
 
 // productOfArray([1, 2, 3, 10])) //60
 // productOfArray([1,2,3]) // 6
