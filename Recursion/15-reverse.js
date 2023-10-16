24eceb9ebcb524b816c0d0e7bd066501330b856a
Ha-Mundo <trauzzu@gmail.com>
2023-10-16 20:05:23 +0100

    Recursion Coding Exercise 15 Added

--- Recursion/15-reverse.js
+++ Recursion/15-reverse.js
@@ -1,6 +1,20 @@
-///////////////////Recursion Coding Exercise 15: reverse////////////////////
+/////////////////// Recursion Coding Exercise 15: reverse ////////////////////
 
-function reverse(str) {}
+function reverse(str) {
+  if (str.length <= 1) return str;
+  return reverse(str.slice(1)) + str[0];
+}
 
-// reverse('awesome') // 'emosewa'
-// reverse('rithmschool') // 'loohcsmhtir'
+reverse("happy");
+/* 
+reverse('happy) 
+
+reverse('appy') + 'h' = 'yppah'
+reverse('ppy') + 'a' = 'yppa'
+reverse('py') + 'p' = 'ypp'
+reverse('y') + 'p' = 'yp'
+reverse('y') = 'y'
+
+*/
+
+// Visual solution --> https://www.youtube.com/watch?v=ExjpYfCB3bM
