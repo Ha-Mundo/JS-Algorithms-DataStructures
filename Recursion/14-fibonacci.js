55ea0f7478e2a1d05fcb159d76dd2560ce061d14
Ha-Mundo <trauzzu@gmail.com>
2023-10-16 15:03:00 +0100

    file name changed

--- Recursion/14-fibonacci.js
+++
@@ -1,5 +0,0 @@
-///////////// Recursion Coding Exercise 14: fibonacci /////////////////
-
-// Write a recursive function called fib which accepts a number and returns the the number in the Fibonacci sequence. Recall that the Fibonacci sequence is the sequence of whole numbers 1, 1, 2, 3, 5, 8, ... which starts with 1 and 1, and where every number thereafter is equal to the sum of the previous two numbers.
-
-function fib(num) {}
