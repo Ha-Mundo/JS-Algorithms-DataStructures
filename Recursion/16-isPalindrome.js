fa930f7f6ef60416aefa8c296734d9462c20a6db
Ha-Mundo <trauzzu@gmail.com>
2023-10-18 19:39:18 +0100

    Recursion Coding Exercise 16 Completed

--- Recursion/16-isPalindrome.js
+++ Recursion/16-isPalindrome.js
@@ -1,6 +1,15 @@
 /////// Recursion Coding Exercise 16: isPalindrome //////////////////////
 
-function isPalindrome(str) {}
+//Write a recursive function called isPalindrome which returns true if the string passes to it is a palindrome (read the same forward and backward). Otherwise return false.
+
+function isPalindrome(str) {
+  if (str.length === 0) return true;
+
+  if (str[0] === str[str.length - 1]) {
+    return isPalindrome(str.slice(1, -1));
+  }
+  return false;
+}
 
 // isPalindrome('tacocat') // true
 // isPalindrome('foobar') // false
