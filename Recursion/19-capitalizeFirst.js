7a1629e8f78fefea233ba4abc8b60ce66527ce23
Ha-Mundo <trauzzu@gmail.com>
2023-10-19 21:33:20 +0100

    Recursion Coding Exercise 19 Completed

--- Recursion/19-capitalizeFirst.js
+++ Recursion/19-capitalizeFirst.js
@@ -2,6 +2,14 @@
 
 // Write a recursive function called capitalizeFirst. Given an array of strings, capitalize the first letter of each string in the array.
 
-function capitalizeFirst(arr) {}
+function capitalizeFirst(arr) {
+  let result = [];
+
+  if (!arr.length) return result;
+
+  result.push(arr[0][0].toUpperCase() + arr[0].slice(1));
+
+  return result.concat(capitalizeFirst(arr.slice(1)));
+}
 
 capitalizeFirst(["car", "taco", "banana"]); // ['Car','Taco','Banana']
