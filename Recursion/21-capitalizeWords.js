044806749624dc2e489449373d0583e7ab260731
Ha-Mundo <trauzzu@gmail.com>
2023-10-20 20:15:04 +0100

    Recursion Coding Exercise 21 Completed

--- Recursion/21-capitalizeWords.js
+++ Recursion/21-capitalizeWords.js
@@ -2,7 +2,15 @@
 
 // Write a recursive function called capitalizeWords. Given an array of words, return a new array containing each word capitalized.
 
-function capitalizeWords(arr) {}
+function capitalizeWords(arr) {
+  let result = [];
+
+  if (!arr.length) return result;
+
+  result.push(arr[0].toUpperCase());
+
+  return [...result, ...capitalizeWords(arr.slice(1))];
+}
 
 const words = ["i", "am", "learning", "recursion"];
-console.log(capitalizeWords(words)); // ['I', 'AM', 'LEARNING', 'RECURSION']
+capitalizeWords(words); // ['I', 'AM', 'LEARNING', 'RECURSION']
