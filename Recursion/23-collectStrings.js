fc3631fb7df721861d7c696f97c95022fc6f2b2e
Ha-Mundo <trauzzu@gmail.com>
2023-10-21 20:38:51 +0100

    Recursion Coding Exercise 23 Added

---
+++ Recursion/23-collectStrings.js
@@ -0,0 +1,21 @@
+////////////  Recursion Coding Exercise 23: collectStrings /////////////
+
+/// Write a function called collectStrings  which accepts an object and returns an array of all the values in the object that have a typeof string
+
+const obj = {
+  stuff: "foo",
+  data: {
+    val: {
+      thing: {
+        info: "bar",
+        moreInfo: {
+          evenMoreInfo: {
+            weMadeIt: "baz",
+          },
+        },
+      },
+    },
+  },
+};
+
+function collectStrings(obj) {}
