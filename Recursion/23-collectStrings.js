630d56bb81cea5e108dd2408c90b7e8974271e4f
Ha-Mundo <trauzzu@gmail.com>
2023-10-21 21:41:13 +0100

    Recursion Coding Exercise 23 Completed

--- Recursion/23-collectStrings.js
+++ Recursion/23-collectStrings.js
@@ -1,6 +1,6 @@
 ////////////  Recursion Coding Exercise 23: collectStrings /////////////
 
-/// Write a function called collectStrings  which accepts an object and returns an array of all the values in the object that have a typeof string
+/// Write a function called collectStrings which accepts an object and returns an array of all the values in the object that have a typeof string
 
 const obj = {
   stuff: "foo",
@@ -18,4 +18,16 @@ const obj = {
   },
 };
 
-function collectStrings(obj) {}
+function collectStrings(obj) {
+  let stringArr = [];
+
+  for (let key in obj) {
+    if (typeof obj[key] === "string") stringArr.push(obj[key]);
+    if (typeof obj[key] === "object")
+      stringArr = stringArr.concat(collectStrings(obj[key]));
+    // if (typeof obj[key] === "object") stringArr.push(...collectStrings(obj[key])) // <-- Instead concat with recursion you can also use push + spread operators and recursion
+  }
+  return stringArr;
+}
+
+collectStrings(obj); // [ 'foo', 'bar', 'baz' ]
