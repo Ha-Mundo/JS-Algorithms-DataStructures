d24b99417c46bedf57c6d1c432f505e83822a1a4
Ha-Mundo <trauzzu@gmail.com>
2023-10-23 20:44:57 +0100

    Search Coding Exercise 25 Added

---
+++ Searching-Algorithms/searchBinary.js
@@ -0,0 +1,11 @@
+////////////  Search Coding Exercise 25: Binary Search Exercise ///////////////
+
+// Write a function called binarySearch  which accepts a sorted  array and a value and returns the index at which the value exists. Otherwise, return -1.
+
+// This algorithm should be more efficient than linearSearch - you can read how to implement it here -
+// https://www.khanacademy.org/computing/computer-science/algorithms/binary-search/a/binary-search and
+// here - https://www.topcoder.com/community/data-science/data-science-tutorials/binary-search/
+
+// Time Complexity - O(log n)
+
+function binarySearch(arr, num) {}
