c28e0489ea982e0cae92435e8d0510aa46c3f214
Ha-Mundo <trauzzu@gmail.com>
2023-10-25 16:49:37 +0100

    Search Coding Exercise 26 Added

---
+++ Searching-Algorithms/naiveStringSearch.js
@@ -0,0 +1,9 @@
+// ///////////// Search Coding Exercise 26: Naive String Search Implementation //////////////
+
+// Write a function which accepts a string and a pattern, and counts the number of times the pattern appears in the string.
+
+// Time Complexity - O(n * m)
+
+function stringSearch(str, val) {}
+
+stringSearch("hojoklokoklok", "lok"); // 2
