a01ba7436d209188addf934d8a0f63e685298574
Ha-Mundo <trauzzu@gmail.com>
2023-11-11 17:21:15 +0000

    Selection Sort Exercise Added

---
+++ sorting-algorithms/selectionSort.js
@@ -0,0 +1,5 @@
+////////////// Selection Sort> Implementation /////////////////
+
+function selectionSort(arr) {}
+
+selectionSort([0, 8, 15, 20, 35, 5, 3, 4, 1, 2]); // [ 0, 1, 2, 3, 4, 5, 8, 15, 20, 35 ]
