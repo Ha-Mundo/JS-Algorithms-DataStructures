67588256bd2eb1b0f3d13d3cb50ab831edc9ddef
Ha-Mundo <trauzzu@gmail.com>
2023-11-11 19:54:01 +0000

    Selection Sort Completed

--- sorting-algorithms/selectionSort.js
+++ sorting-algorithms/selectionSort.js
@@ -1,5 +1,21 @@
 ////////////// Selection Sort> Implementation /////////////////
 
-function selectionSort(arr) {}
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let lowestNumIndex = i;
+
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[lowestNumIndex]) lowestNumIndex = j;
+    }
+
+    if (lowestNumIndex !== i) {
+      let temp = arr[i];
+      arr[i] = arr[lowestNumIndex];
+      arr[lowestNumIndex] = temp;
+    }
+  }
+
+  return arr;
+}
 
 selectionSort([0, 8, 15, 20, 35, 5, 3, 4, 1, 2]); // [ 0, 1, 2, 3, 4, 5, 8, 15, 20, 35 ]
