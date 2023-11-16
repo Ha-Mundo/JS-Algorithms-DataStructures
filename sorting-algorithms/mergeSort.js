e940fb5ebfd1c3eee2c72936f39e36d4d5af3481
Ha-Mundo <trauzzu@gmail.com>
2023-11-16 20:27:21 +0000

    Merge Sort Function Completed

--- sorting-algorithms/mergeSort.js
+++ sorting-algorithms/mergeSort.js
@@ -40,8 +40,19 @@ merge([100,200], [1, 2, 3, 5, 6]); // [1, 2, 3, 5, 6, 100, 200]
 /* 
 mergeSort Pseudocode:
 - Break up the array into halves until you have arrays that are empty or have one element
-- Once you have smaller sorted arrays, merge those arrays with other sorted arrays until         you are back at the full length of the array
+- Once you have smaller sorted arrays, merge those arrays with other sorted arrays until you are back at the full length of the array
 -Once the array has been merged back together, return the merged (and sorted!) array
 */
 
-function mergeSort(arr) {}
+function mergeSort(arr) {
+    if(arr.length <= 1) return arr
+    
+    let center = Math.floor(arr.length / 2)
+    let left = arr.slice(0, center)
+    let right = arr.slice(center)
+    
+    return merge(mergeSort(left), mergeSort(right));
+    }
+
+mergeSort([10, 24, 76, 73]); //[10, 24, 73, 76]
+                
