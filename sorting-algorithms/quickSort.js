12ea6d86eb524c35bd616b0f310141cc3498ab50
Ha-Mundo <trauzzu@gmail.com>
2023-11-28 18:20:42 +0000

    quickSort Function Refactored

--- sorting-algorithms/quickSort.js
+++ sorting-algorithms/quickSort.js
@@ -1,27 +1,19 @@
 //////////////  Quick Sort Implementation  ///////////////
 
 function quickSort(arr) {
-    if ( arr.length === 1) return arr // Base case 
+    if ( arr.length <= 1) return arr // Base Case 
 
     const pivot = arr[arr.length - 1]
     const leftArr = []
     const rightArr = []
-
-    for (let i = 0; i < arr.length - 1; i++) {
-        if (arr[i] < pivot) {
-            leftArr.push(arr[i])
-        } else {
-            rightArr.push(arr[i])
-        }
+    // With the slice method we create swallow copy of the original array that keep our function pure without including last(Pivot) element of the original array
+    for(const el of arr.slice(0, arr.length - 1)) {
+        el < pivot ? leftArr.push(el) : rightArr.push(el)
     }
 
-    if (leftArr.length > 0 && rightArr.length > 0) {
-        return [...quickSort(leftArr), pivot, ...quickSort(rightArr)]
-    } else if (leftArr.length > 0) {
-        return [...quickSort(leftArr), pivot]
-    } else {
-        return [pivot, ...quickSort(rightArr)]
-    }
+    
+    return [...quickSort(leftArr), pivot, ...quickSort(rightArr)]
+    
 }
 
 quickSort([-2, 5, -6, 1, 3]); //[-6, -2, 1, 3, 5]
