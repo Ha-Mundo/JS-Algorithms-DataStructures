ecd3dfa147be08a28d3a8c571a23000e80612523
Ha-Mundo <trauzzu@gmail.com>
2023-12-12 12:16:12 +0000

    Stack Info Added

--- Data-Structures/Stack/stack.js
+++ Data-Structures/Stack/stack.js
@@ -1,3 +1,19 @@
+/*//////////////////////// Stack ///////////////////////////////////
+ 
+Abstract data structure of a collection of data that abides by LIFO (last In - First Out).
+
+Push method takes in a node and puts it at the top of the stack should return the new size of the stack. Pop method removes the node at the top of the stack and returns the value of that node.
+
+- Managing function invocations
+- Undo/Redo
+- Routing (history in a browser)
+
+Insertion: O(1)
+Removal: O(1)
+Searching: O(n)
+Access: O(n)
+ */
+
 class Node {
     constructor(val, next = null) {
       this.val = val;
@@ -22,15 +38,28 @@ class Node {
       }
 
     pop() {
-    if (!this.first) return null;
+       if (!this.first) return null;
 
-    const removedNode = this.first;
-    this.first = removedNode.next;
-    this.size--;
+       const removedNode = this.first;
+       this.first = removedNode.next;
+       this.size--;
 
-    if (!this.size) this.last = null;
+       if (!this.size) this.last = null;
 
-    return removedNode.data;
+       return removedNode.val;
       }
     
 }
+
+let stack = new Stack()
+
+stack.push(10);
+stack.push(100);
+stack.push(1000);
+console.log(stack);
+console.log(stack.pop()); // 1000
+console.log(stack.size); // 2
+stack.pop();
+stack.pop();
+console.log(stack.size); // 0
+console.log(stack.pop()); // null
