2dcb6362168eadda97376ff383730fafe96f6357
Ha-Mundo <trauzzu@gmail.com>
2023-12-12 19:59:40 +0000

    Enqueue Method Added

--- Data-Structures/Queue/queue.js
+++ Data-Structures/Queue/queue.js
@@ -5,10 +5,22 @@ class Node {
     }
   };
 
-  class Queue {
+class Queue {
     constructor () {
       this.first = null;
       this.last = null;
       this.size = 0;
     }
+
+    enqueue(val) {
+        const newNode = new Node(val);
+    
+        if (!this.first) this.first = newNode;
+        else this.last.next = newNode;
+    
+        this.last = newNode;
+        this.size++;
+    
+        return this.size;
+      }
 }
