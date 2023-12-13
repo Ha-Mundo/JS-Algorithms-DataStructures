9b682892b67dce48d0ac0b6764e81267f6f41398
Ha-Mundo <trauzzu@gmail.com>
2023-12-13 19:44:08 +0000

    Binary Search Tree Insert Method Added

--- Data-Structures/Binary-Search-Tree/binary-search-tree.js
+++ Data-Structures/Binary-Search-Tree/binary-search-tree.js
@@ -10,4 +10,44 @@ class Node {
     constructor() {
       this.root = null;
     }
+
+    insert(val) {
+        let newNode = new Node(val)
+
+        if(!this.root) {
+            this.root = newNode
+            return this
+        } 
+        let currentNode = this.root
+
+        while(true) {
+            if(val === currentNode.val) return undefined
+            if(val < currentNode.val) {
+                if(!currentNode.left) {
+                    currentNode.left = newNode
+                    return this
+                }
+                currentNode = currentNode.left
+            } else {
+                if(val > currentNode.val) {
+                    if(!currentNode.right){
+                        currentNode.right = newNode
+                        return this
+                    }
+                    currentNode = currentNode.right
+                }
+            }
+        }
+    }
 }
+
+let tree = new BinarySearchTree()
+
+tree.insert(10)
+tree.insert(5)
+tree.insert(13)
+tree.insert(2)
+tree.insert(7)
+tree.insert(11)
+tree.insert(16)
+console.log(tree);
