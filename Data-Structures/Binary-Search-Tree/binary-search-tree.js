d1fe054da92838cf35dee9ce43619900496dfd08
Ha-Mundo <trauzzu@gmail.com>
2023-12-14 18:23:53 +0000

    Code Updated

--- Data-Structures/Binary-Search-Tree/binary-search-tree.js
+++ Data-Structures/Binary-Search-Tree/binary-search-tree.js
@@ -117,14 +117,14 @@ class Node {
     }
 }
 
-let tree = new BinarySearchTree()
+let tree = new BinarySearchTree();
 
-tree.insert(10)       
-tree.insert(6)       
-tree.insert(15)     
-tree.insert(3)
-tree.insert(8)
-tree.insert(20)
+tree.insert(10);      
+tree.insert(6);       
+tree.insert(15);     
+tree.insert(3);
+tree.insert(8);
+tree.insert(20);
 
 
 console.log(tree); // return the tree
