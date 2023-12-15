f0a9a118d468440d8d202b01e123407a350e1be5
Ha-Mundo <trauzzu@gmail.com>
2023-12-15 18:30:01 +0000

    bubbleUp Method Added

--- Data-Structures/Binary-Heaps/Max-Binary-Heap.js
+++ Data-Structures/Binary-Heaps/Max-Binary-Heap.js
@@ -15,5 +15,27 @@ class MaxBinaryHeap {
 
     getParentIndex(childIndex) {
         return Math.floor((childIndex - 1) / 2);
-      }
+    }
+
+    bubbleUp(idx = this.values.length - 1) {
+    let index = idx; 
+    const element = this.getItem(index);
+    
+    while(index > 0) {
+        let parentIndex =  this.getParentIndex(index); 
+        let parentElement = this.getItem(parentIndex);
+        
+        if(element <= parentElement) break;
+        
+        this.values[parentIndex] = element;
+        this.values[index] = parentElement;
+        index = parentIndex;
+        }
+    }
 }
+
+let array = [41, 39, 33, 18, 27, 12]
+
+let heap = new MaxBinaryHeap(array)
+heap.insert(55)
+console.log(heap.values);
