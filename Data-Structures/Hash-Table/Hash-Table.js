5aff0e1bb8a3ef8926d08f207a816467d96a1a76
Ha-Mundo <trauzzu@gmail.com>
2023-12-22 20:10:44 +0000

    Delete Method Added

--- Data-Structures/Hash-Table/Hash-Table.js
+++ Data-Structures/Hash-Table/Hash-Table.js
@@ -39,6 +39,20 @@ class HashTable {
         return undefined;
     }
 
+    delete(key) {
+        const index = this.hash(key);
+    
+        if (this.keyMap[index]) {
+          for (let i = 0; i < this.keyMap[index].length; i++) {
+            if (this.keyMap[index][i][0] === key) {
+              return [].concat(...this.keyMap[index].splice(i, 1));
+            }
+          }
+        }
+    
+        return null;
+    }
+
     keys() {
         const keys = [];
     
@@ -90,17 +104,16 @@ hashTable.set('mediumvioletred', '#C71585');
 hashTable.set('plum', '#DDA0DD');
 hashTable.set('lightcoral', '#EE7F74');
 hashTable.set('coral', '#EE7F74');
-
 console.log(hashTable.get('plum')); // '#DDA0DD'
 console.log(hashTable.values()); // [ '#DDA0DD','#FA8072','#800000','#FFFF00','#EE7F74','#808000','#F08080','#C71585' ]
 console.log(hashTable.keys()); // [ 'plum','salmon','maroon','yellow','coral','olive','lightcoral','lightcoral','mediumvioletred' ]
+console.log(hashTable.delete('lightcoral')); // [ 'lightcoral', '#F08080' ]
 hashTable.print();/*'bucket 0: plum,#DDA0DD' 
                     'bucket 3: salmon,#FA8072' 
                     'bucket 8: maroon,#800000' 
                     'bucket 8: yellow,#FFFF00' 
                     'bucket 9: coral,#EE7F74' 
                     'bucket 10: olive,#808000' 
-                    'bucket 13: lightcoral,#F08080' 
                     'bucket 13: lightcoral,#EE7F74' 
                     'bucket 16: mediumvioletred,#C71585'
                     */
