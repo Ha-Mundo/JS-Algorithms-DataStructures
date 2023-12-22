ff8648e1631aa5a70c117c4f08bf703b2fca0bd8
Ha-Mundo <trauzzu@gmail.com>
2023-12-22 16:45:41 +0000

    Hash Table Class Added

--- Data-Structures/Hash-Table/Hash-Table.js
+++ Data-Structures/Hash-Table/Hash-Table.js
@@ -1,10 +1,17 @@
-function hash(key, arrayLength) {
-    let total = 0;
-    let WEIRD_PRIME = 31;
-    for (let i = 0; i < Math.min(key.length, 100); i++) {
-        let char = key[i];
-        let value = char.charCodeAt(0) - 96;
-        total = (total + WEIRD_PRIME + value) % arrayLength;
+class HashTable {
+    constructor (size = 53) {
+      this.keyMap = new Array(size);
     }
-  return total;
-  }
+
+    hash(key, arrayLength) {
+        let total = 0;
+        let WEIRD_PRIME = 31;
+        for (let i = 0; i < Math.min(key.length, 100); i++) {
+            let char = key[i];
+            let value = char.charCodeAt(0) - 96;
+            total = (total + WEIRD_PRIME + value) % arrayLength;
+        }
+    return total;
+    }
+
+}
