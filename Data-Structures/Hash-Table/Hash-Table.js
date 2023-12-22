93b67c765000781784c997959cd9fb38c4bcc1c6
Ha-Mundo <trauzzu@gmail.com>
2023-12-22 18:08:59 +0000

    Get Method Added

--- Data-Structures/Hash-Table/Hash-Table.js
+++ Data-Structures/Hash-Table/Hash-Table.js
@@ -4,38 +4,53 @@ class HashTable {
       }
     
     hash(key) {
-    let hash = 0;
-    const WEIRD_PRIME = 31;
+        let hash = 0;
+        const WEIRD_PRIME = 31;
 
-    for (let i = 0; i < Math.min(key.length, 100); i++) {
-        const char = key[i];
-        const value = char.charCodeAt(0) - 96;
-        hash = (hash * WEIRD_PRIME + value) % this.keyMap.length;
-        }
+        for (let i = 0; i < Math.min(key.length, 100); i++) {
+            const char = key[i];
+            const value = char.charCodeAt(0) - 96;
+            hash = (hash * WEIRD_PRIME + value) % this.keyMap.length;
+            }
 
-    return hash;
+        return hash;
     }
     
 
     set(key, value) {
         let index = this.hash(key);
-        console.log(index);
+
         if (!this.keyMap[index]) {
           this.keyMap[index] = [];
         }
         this.keyMap[index].push([key, value]);
-      }
+    }
+
+    get(key) {
+        const index = this.hash(key);
+
+        if (this.keyMap[index]) {
+            for (const item of this.keyMap[index]) {
+                if (item[0] === key) return item[1];
+                    
+                }
+            }
+
+        return undefined;
+    }
 
 }
 
-const hashtable = new Hashtable(17);
+const hashTable = new HashTable(17);
 
-hashtable.set('maroon', '#800000');
-hashtable.set('yellow', '#FFFF00');
-hashtable.set('olive', '#808000');
-hashtable.set('salmon', '#FA8072');
-hashtable.set('lightcoral', '#F08080');
-hashtable.set('mediumvioletred', '#C71585');
-hashtable.set('plum', '#DDA0DD');
-hashtable.set('lightcoral', '#EE7F74');
-hashtable.set('coral', '#EE7F74');
+hashTable.set('maroon', '#800000');
+hashTable.set('yellow', '#FFFF00');
+hashTable.set('olive', '#808000');
+hashTable.set('salmon', '#FA8072');
+hashTable.set('lightcoral', '#F08080');
+hashTable.set('mediumvioletred', '#C71585');
+hashTable.set('plum', '#DDA0DD');
+hashTable.set('lightcoral', '#EE7F74');
+hashTable.set('coral', '#EE7F74');
+
+console.log(hashTable.get('plum')); // '#DDA0DD'
