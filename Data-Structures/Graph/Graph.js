5f278b3a61bb99ae5630f7fc082ebe7372228379
Ha-Mundo <trauzzu@gmail.com>
2023-12-24 11:27:47 +0000

    addVertex Method Added

--- Data-Structures/Graph/Graph.js
+++ Data-Structures/Graph/Graph.js
@@ -4,4 +4,16 @@ class Graph {
     constructor() {
         this.adjacencyList = {};
     }
+
+    addVertex(vertex) {
+        if(!this.adjacencyList[vertex]) this.adjacencyList[vertex] = []
+    }
 }
+
+const graph = new Graph()
+
+graph.addVertex('Tokio')
+graph.adjacencyList['Tokio'].push('Japan')
+graph.addVertex('Tokio')
+
+console.log(graph);
