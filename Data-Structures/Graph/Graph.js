8db50eb86b99e3fc99b2b3b6df962f7fe3654cb1
Ha-Mundo <trauzzu@gmail.com>
2023-12-24 16:24:48 +0000

    removeVertex Method Added

--- Data-Structures/Graph/Graph.js
+++ Data-Structures/Graph/Graph.js
@@ -6,18 +6,28 @@ class Graph {
     }
 
     addVertex(vertex) {
-        if(!this.adjacencyList[vertex]) this.adjacencyList[vertex] = []
+        if(!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
     }
 
     addEdge(vertex1, vertex2) {
-        this.adjacencyList[vertex1].push(vertex2) 
-        this.adjacencyList[vertex2].push(vertex1)
+        this.adjacencyList[vertex1].push(vertex2);
+        this.adjacencyList[vertex2].push(vertex1);
     }
 
     removeEdge(vertex1, vertex2) {
-     this.adjacencyList[vertex1] =  this.adjacencyList[vertex1].filter(v => v !== vertex2 )
-     this.adjacencyList[vertex2] =  this.adjacencyList[vertex2].filter(v => v !== vertex1 )
+     this.adjacencyList[vertex1] =  this.adjacencyList[vertex1].filter(v => v !== vertex2);
+     this.adjacencyList[vertex2] =  this.adjacencyList[vertex2].filter(v => v !== vertex1);
     }
+
+    removeVertex(vertex) {
+        if (this.adjacencyList[vertex]) {
+            while(this.adjacencyList[vertex].length) {
+                this.removeEdge(vertex, this.adjacencyList[vertex].pop());
+                }
+            delete this.adjacencyList[vertex];
+            }
+        }
+
 }
 
 const graph = new Graph()
@@ -32,6 +42,8 @@ graph.addEdge('Tokyo', 'Dallas')
 graph.addEdge('Rome', 'Dallas')
 graph.addEdge('Rome', 'Paris')
 
-graph.removeEdge('Rome', 'Paris')
+//graph.removeEdge('Rome', 'Paris')
 
-console.log(graph);
+console.log(graph.adjacencyList); // { Tokyo: [ 'Rome', 'Dallas' ], Dallas: [ 'Tokyo', 'Rome' ], Rome: [ 'Tokyo', 'Dallas', 'Paris' ], Paris: [ 'Rome' ] }
+graph.removeVertex('Rome')
+console.log(graph.adjacencyList); // { Tokyo: [ 'Dallas' ], Dallas: [ 'Tokyo' ], Paris: [] }
