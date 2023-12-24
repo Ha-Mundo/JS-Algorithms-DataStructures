5a9bc8b34e4937b4b6117ced5c3ab9b4f14aacdd
Ha-Mundo <trauzzu@gmail.com>
2023-12-24 11:54:46 +0000

    addEdge Method Added

--- Data-Structures/Graph/Graph.js
+++ Data-Structures/Graph/Graph.js
@@ -8,12 +8,23 @@ class Graph {
     addVertex(vertex) {
         if(!this.adjacencyList[vertex]) this.adjacencyList[vertex] = []
     }
+
+    addEdge(v1, v2) {
+        this.adjacencyList[v1].push(v2) 
+        this.adjacencyList[v2].push(v1)
+    }
 }
 
 const graph = new Graph()
 
-graph.addVertex('Tokio')
-graph.adjacencyList['Tokio'].push('Japan')
-graph.addVertex('Tokio')
+graph.addVertex('Tokyo')
+graph.addVertex('Dallas')
+graph.addVertex('Rome')
+graph.addVertex('Paris')
+
+graph.addEdge('Tokyo', 'Rome')
+graph.addEdge('Tokyo', 'Dallas')
+graph.addEdge('Rome', 'Dallas')
+graph.addEdge('Rome', 'Paris')
 
 console.log(graph);
