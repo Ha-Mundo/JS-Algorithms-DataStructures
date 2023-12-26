d78f2c477e47aca8e0dee8054900fe7552f05db3
Ha-Mundo <trauzzu@gmail.com>
2023-12-26 16:15:59 +0000

    Dijkstra Initial State Added

--- Data-Structures/Dijkstra's Algorithm/Weighted-Graph.js
+++ Data-Structures/Dijkstra's Algorithm/Weighted-Graph.js
@@ -29,6 +29,24 @@ class WeightedGraph {
         this.adjacencyList[vertex1].push({ node: vertex2, weight });
         this.adjacencyList[vertex2].push({ node: vertex1, weight });
       }
+
+    Dijkstra(start, finish){
+        const nodes = new PriorityQueue();
+        const distances = {};
+        const previous = {};
+
+        // Build the initial state
+        for(let vertex in this.adjacencyList) {
+            if(vertex === start) {
+                distances[vertex] = 0;
+                nodes.enqueue(vertex, 0);
+            } else {
+                distances[vertex] = Infinity;
+                nodes.enqueue(vertex, Infinity);
+            }
+            previous[vertex] = null;
+        }
+    }
 }
 
 const graph = new WeightedGraph();
