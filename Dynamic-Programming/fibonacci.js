67f53ab4c92872689eff7d6dd971ac3fc52ccf9f
Ha-Mundo <trauzzu@gmail.com>
2023-12-27 18:23:02 +0000

    Brute Force Sample updated

--- Dynamic-Programming/fibonacci.js
+++ Dynamic-Programming/fibonacci.js
@@ -1,11 +1,16 @@
 /* Fibonacci
-Write a recursive function called fib which accepts a number and returns the nth number in the Fibonacci sequence using Dynamic Programming approach.
-Recall that the Fibonacci sequence is the sequence of whole numbers 1, 1, 2, 3, 5, 8, ... which starts with 1 and 1, and where every number thereafter is equal to the sum of the previous two numbers.
+ Write a recursive function called fib which accepts a number and returns the the number in the Fibonacci sequence using Dynamic Programming approach. 
+ Recall that the Fibonacci sequence is the sequence of whole numbers 0, 1, 1, 2, 3, 5, 8, ... which starts with 0 and 1, and where every number thereafter is equal to the sum of the previous two numbers.
+
+ 0, 1, 2, 3, 4, 5, 6, 7 <-- numbers
+ 0, 1, 1, 2, 3, 5, 8, 13 <-- fibonacci numbers
  */
 
 // Brute Force
-// THIS IS BAD. O(2^) Time Complexity
-function fibBF(n) {
-    if (n <= 2) return 1;
-    return fib(n - 1) + fib(n - 2);
-  }
+// Time Complexity O(2^n) <-- This is Bad
+function fibBF(num) {
+    if (num < 2) return num;
+    return fibBF(num - 1) + fibBF(num - 2);
+}
+
+
