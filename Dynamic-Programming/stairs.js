adc141866c314befda26bfaad80474bab702a8c4
Ha-Mundo <trauzzu@gmail.com>
2023-12-28 16:09:28 +0000

    Stairs Exercise Added

---
+++ Dynamic-Programming/stairs.js
@@ -0,0 +1,8 @@
+/*
+ Write a function called stairs which accepts n number of stairs. Imagine that a person is standing at the bottom of the stairs and wants to reach the top and the person can climb either 1 stair or 2 stairs at a time. Your function should return the number of ways the person can reach the top by only climbing 1 or 2 stairs at a time. 
+ Use Dynamic Programming approach.
+ */
+
+ // Brute Force
+// Time Complexity O(2^n)
+function stairsBF(num) {}
