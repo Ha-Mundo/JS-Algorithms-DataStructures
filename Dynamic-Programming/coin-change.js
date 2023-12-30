eb770d908761f363d1c9de3ee54869cb98608663
Ha-Mundo <trauzzu@gmail.com>
2023-12-30 17:56:01 +0000

    Top-down Function Reference Sample Added

--- Dynamic-Programming/coin-change.js
+++ Dynamic-Programming/coin-change.js
@@ -22,3 +22,34 @@ function coinChangeBF(denominations, value) {
   
   console.log(coinChangeBF([1, 5, 10, 25], 50)); // 49
   console.log(coinChangeBF([1, 5, 10, 25], 7)); // 2
+
+  ///////////////////////////////////// Dynamic Programming ///////////////////////////////////////
+
+// Top-down approach - Memoization
+
+// Time Complexity O(value * denominations.length)
+// Space Complexity O(value * denominations.length)
+
+function coinChangeTD(denominations, value) {
+    const index = denominations.length - 1;
+    const memo = {};
+
+    function change(denominations, value, index) {
+        if (value < 0 || index < 0) return 0;
+        if (value === 0) return 1;
+        
+        const key = `${index}/${value}`;
+        
+        if (memo[key]) return memo[key];
+        
+        memo[key] = change(denominations, value - denominations[index], index) +
+        change(denominations, value, index - 1);
+        
+        console.log(memo);
+      return memo[key];
+    }
+  
+    return change(denominations, value, index);
+  }
+
+console.log(coinChangeTD([1, 5, 10, 25], 14000)); // 367633141
